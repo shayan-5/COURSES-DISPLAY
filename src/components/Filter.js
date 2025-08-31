00cc35ab623cd28c698c684d0c705eaa37bd8e4d
shayan-5 <shaikhshayan5802@gmail.com>
2025-08-31 16:30:04 +0530

    Toast and like button removed

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -17,7 +17,7 @@ const Filter = (props) => {
             className={`text-lg px-2 py-1 rounded-md font-medium 
             text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300
             ${category === data.title ? 
-            "bg-opacity-60 border-white" :
+            " bg-white text-black" :
             "bg-opacity-40 border-transparent"}
             `}
              key={data.id}
